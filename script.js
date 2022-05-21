394c5021fe54ed5815714477b20985d43c84c05b
Andrew <echowhite93@yahoo.com>
2022-05-20 19:13:34 -0700

    added quiz buttons, quiz buttons styling, and quiz questions

---
+++ script.js
@@ -0,0 +1,37 @@
+// object containing questions
+var  quizQuestions = [{
+    question: "Which built-in method returns the calling string value converted to upper case?",
+    choiceA: "toUpperCase()",
+    choiceB: "toUpper()",
+    choiceC: "changeCase(case)",
+    choiceD: "None of the above",
+    correctAnswer: "a"},
+{
+    question: "Which of the following type of variables is visible everywhere in your JavaScript code?",
+    choiceA: "Local Variable",
+    choiceB: "Global Variable",
+    choiceC: "Universal Variable",
+    choiceD: "None of the above",
+    correctAnswer: "b"},
+{
+    quesion: "How may elements can you apply an 'ID' attribute to?",
+    choiceA: "2",
+    choiceB: "3",
+    choiceC: "Unlimited",
+    choiceD: "1",
+    correctAnswer: "d"},
+{
+    quesion: "Which is NOT a Javascript operator?",
+    choiceA: "===",
+    choiceB: "!==",
+    choiceC: "@@",
+    choiceD: "<=",
+    correctAnswer: "c"},
+{
+    quesion: "Which is a JavaScript Data type?",
+    choiceA: "Number",
+    choiceB: "String",
+    choiceC: "Object",
+    choiceD: "All of the above",
+    correctAnswer: "d"},
+];
